60b3ba4514d4f327d2b1399fd37b96576bd4d846
zamba.cl <carolzambanini6@gmail.com>
2026-06-18 09:56:37 -0300

    refactor: alterações nos layouts

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,33 +1,35 @@
 import { Tabs } from 'expo-router';
-import React from 'react';
+import { BookOpen, CakeSlice, Pizza } from 'lucide-react-native'
 
-import { HapticTab } from '@/components/haptic-tab';
-import { IconSymbol } from '@/components/ui/icon-symbol';
-import { Colors } from '@/constants/theme';
-import { useColorScheme } from '@/hooks/use-color-scheme';
-
-export default function TabLayout() {
-  const colorScheme = useColorScheme();
+export default function Layout() {
 
   return (
-    <Tabs
-      screenOptions={{
-        tabBarActiveTintColor: Colors[colorScheme ?? 'light'].tint,
-        headerShown: false,
-        tabBarButton: HapticTab,
-      }}>
+    <Tabs screenOptions={{
+      sceneStyle: { backgroundColor: '#fffaf0', },
+      headerShown: false,
+      tabBarStyle: {   backgroundColor: '#fffaf0', borderTopColor: '#e8dcc6', },
+      tabBarActiveTintColor: '#e2da00',
+      tabBarInactiveTintColor: '#c7b299',
+    }}>
       <Tabs.Screen
         name="index"
         options={{
-          title: 'Home',
-          tabBarIcon: ({ color }) => <IconSymbol size={28} name="house.fill" color={color} />,
+          title: '',
+          tabBarIcon: ({color}) => <BookOpen size={28} color={color} />,
         }}
       />
       <Tabs.Screen
-        name="explore"
+        name="doces"
         options={{
-          title: 'Explore',
-          tabBarIcon: ({ color }) => <IconSymbol size={28} name="paperplane.fill" color={color} />,
+          title: '',
+          tabBarIcon: ({color}) => <CakeSlice size={28} color={color} />,
+        }}
+      />
+      <Tabs.Screen
+        name="salgados"
+        options={{
+          title: '',
+          tabBarIcon: ({color}) => <Pizza size={28} color={color} />,
         }}
       />
     </Tabs>
